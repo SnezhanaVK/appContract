4205ddbd023ca6093002196144f8b25fd496426c
Snezhana Kudryavtseva <snezhana.kydr@mail.ru>
2025-06-21 00:11:53 +0700

    change cors

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -7,6 +7,8 @@ import (
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3001")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3002")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
